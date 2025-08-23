1046e2f0fcbc32a09db75e9fcc5f290c16dc7617
Luka Drobnjak <159907553+Marchelo@users.noreply.github.com>
2025-08-23 15:47:39 +0200

    Created Transaction route
    Beggining of routing...

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ dotenv.config();
 
 const app = express();
 
+// middleware - fja koja se odvija izmedju request-a i response-a
+app.use(express.json());
+
 const PORT = process.env.PORT || 5001;
 
 async function initDB() {
@@ -27,9 +30,29 @@ async function initDB() {
   }
 }
 
-app.get("/", (req, res) => {
-    res.send("It's working."); 
-});
+app.post("/api/transactions", async (req, res) => {
+  // title amount catrgort userID
+  try {
+    // destructure
+    const {title, amount, category, user_id} = req.body;
+    if (!title || !user_id || !category || amount === undefined) {
+      return res.status(400).json({message: "All fields are required!"});
+    }
+
+    const transaction = await sql`
+      INSERT INTO transactions(user_id, title, amount, category)
+      VALUES (${user_id},${title},${amount},${category})
+      RETURNING *
+    `
+    
+    console.log(transaction)
+    res.status(201).json(transaction[0])
+
+  } catch (error) {
+    console.log("Error creating transaction", error);
+    res.status(500).json({message: "Internal error"})
+  }
+})
 
 console.log("my port:", process.env.PORT);
 
