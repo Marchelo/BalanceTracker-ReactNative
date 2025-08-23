372376d94f77bccb07e0fd0634c4f0a1ad1802cd
Luka Drobnjak <159907553+Marchelo@users.noreply.github.com>
2025-08-23 15:14:39 +0200

    Database setup
    Connected neon data base with my server.js

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 // const express = require("express")
 import express from "express"
 import dotenv from "dotenv"
+import { sql } from "./config/db.js";
 
 dotenv.config();
 
@@ -8,12 +9,32 @@ const app = express();
 
 const PORT = process.env.PORT || 5001;
 
+async function initDB() {
+  try {
+    await sql`CREATE TABLE IF NOT EXISTS transactions(
+      id SERIAL PRIMARY KEY,
+      user_id VARCHAR(255) NOT NULL,
+      title  VARCHAR(255) NOT NULL,
+      amount  DECIMAL(10,2) NOT NULL,
+      category VARCHAR(255) NOT NULL,
+      created_at DATE NOT NULL DEFAULT CURRENT_DATE
+    )`;
+    // max value in DECIMAL(10,2) - 99999999.99 
+    console.log("Database initialized successfully");
+  } catch (error) {
+    console.log("Error initializing DB", error);
+    process.exit(1); // status code 1 means failure, 0 success
+  }
+}
+
 app.get("/", (req, res) => {
     res.send("It's working."); 
 });
 
 console.log("my port:", process.env.PORT);
 
-app.listen(PORT, () => {
-    console.log("Server is up and running on PORT:", PORT)
+initDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server is up and running on PORT:", PORT);
+  });
 });
