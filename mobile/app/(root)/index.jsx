bb3db93acbcad00508cfdd7479f190047195a897
Luka Drobnjak <159907553+Marchelo@users.noreply.github.com>
2025-08-24 23:59:56 +0200

    Home Screen
    Finished home screen, except button 'add'

--- mobile/app/(root)/index.jsx
+++ mobile/app/(root)/index.jsx
@@ -1,46 +1,99 @@
 import { SignedIn, SignedOut, useUser } from '@clerk/clerk-expo'
-import { Link } from 'expo-router'
-import { Text, View } from 'react-native'
+import { Link, useRouter } from 'expo-router'
+import { Alert, FlatList, Image, RefreshControl, Text, TouchableOpacity, View } from 'react-native'
 import { SignOutButton } from '@/components/SignOutButton'
 import { useTransactions } from '../../hooks/useTransaction'
 import { useEffect } from 'react'
+import PageLoader from '../../components/PageLoader'
+import { styles } from '../../assets/styles/home.styles'
+import { Ionicons } from "@expo/vector-icons";
+import { BalanceCard } from '../../components/BalanceCart'
+import { TransactionItem } from '../../components/TransactionItem'
+import NoTransactionsFound from '../../components/NoTransactionsFound'
+import { useState } from 'react'
+
 
 export default function Page() {
   const { user } = useUser()
+  const router = useRouter();
+  const [refreshing, setRefreshing] = useState(false);
 
-  const {
-    transactions,
-    summary,
-    isLoading,
-    loadData,
-    deleteTransaction
-  } = useTransactions(user.id)
+  const { transactions, summary, isLoading, loadData, deleteTransaction } = useTransactions(
+    user.id
+  );
+
+  const onRefresh = async () => {
+    setRefreshing(true);
+    loadData();
+    setRefreshing(false);
+  }
 
   useEffect(()=>{
     loadData();
   }, [loadData]);
 
-  console.log("transactions:", transactions)
-  console.log("summary:", summary)
 
+  const handleDelete = (id) => {
+    Alert.alert("Delete transaction", "Are you sure you want to delete transaction?",[
+      { text: 'Cancel', style:'cancel' },
+      { text: 'Delete', style:'destructive', onPress: () => deleteTransaction(id)},
+    ]);
+  }
+
+  if(isLoading && !refreshing) return <PageLoader />;
 
   return (
-    <View>
-      <SignedIn>
-        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
-        <Text>Balance: {summary.balance}</Text>
-        <Text>Income: {summary.income}</Text>
-        <Text>Expenses: {summary.expenses}</Text>
-        <SignOutButton />
-      </SignedIn>
-      <SignedOut>
-        <Link href="/(auth)/sign-in">
-          <Text>Sign in</Text>
-        </Link>
-        <Link href="/(auth)/sign-up">
-          <Text>Sign up</Text>
-        </Link>
-      </SignedOut>
+    <View style={styles.container}>
+      <View style={styles.content}>
+        {/* HEADER */}
+        <View style={styles.header}>
+
+          {/* LEFT */}
+          <View style={styles.headerLeft}>
+            <Image
+              source={require("../../assets/images/logo.png")}
+              style={styles.headerLogo}
+              resizeMode="contain"
+            />
+            <View style={styles.welcomeContainer}>
+              <Text style={styles.welcomeText}>Welcome,</Text>
+              <Text style={styles.usernameText}>
+                {user?.emailAddresses[0]?.emailAddress.split("@")[0]}
+              </Text>
+            </View>
+          </View>
+
+          {/* RIGHT */}
+          <View style={styles.headerRight}>
+            <TouchableOpacity style={styles.addButton} onPress={() => router.push("/create")}>
+              <Ionicons name="add" size={20} color="#FFF" />
+              <Text style={styles.addButtonText}>Add</Text>
+            </TouchableOpacity>
+            <SignOutButton />
+          </View>
+        </View>
+
+        <BalanceCard summary={summary} />
+
+        <View style={styles.transactionsHeaderContainer}>
+          <Text style={styles.sectionTitle}>Recent Transactions</Text>
+        </View>
+
+      </View>
+
+{/* FlatList - bolji nacin za renderovanje dugih lista u React Native. 
+Renderuje ih 'lazily' samo one na ekranu, npr imas 500 transakcija, na ekranu se prikazuje samo 5 koji mogu stati na ekran  
+renderovace se ostale tek kada skrolujes dole kroz listu transakcija*/}
+      <FlatList 
+        style={styles.transactionsList}
+        contentContainerStyle={styles.transactionsListContent}
+        data={transactions}
+        renderItem={({item}) => <TransactionItem item={item} onDelete={handleDelete} />}
+        ListEmptyComponent={<NoTransactionsFound />}
+        showsVerticalScrollIndicator={false}
+        refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefresh} />}
+      />
+
     </View>
   )
 }
